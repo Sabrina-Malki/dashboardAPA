a6cc0d0594ea30f5b7ee09b1dc7fd14046ca5254
Sabrina <fs_malki@esi.dz>
2019-08-21 21:08:53 +0100

    MAJ RessourceController

--- src/main/java/dashboard/controller/RessourceController.java
+++ src/main/java/dashboard/controller/RessourceController.java
@@ -1,8 +1,12 @@
 package dashboard.controller;
 
+import dashboard.repository.RessourceRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 public class RessourceController {
 
-    @RequestMapping("/")
+    @Autowired
+    RessourceRepo resRepo;
+
 }
